0f53fe77ec0c488e7cf21e39f5f7370149520b48
Markus <mark394e@stud.kea.dk>
2022-11-09 13:45:40 +0100

    thank you note

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,38 +1,49 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { insertOrder } from "./modules/db";
 
 function CheckoutForm(props) {
   const theForm = useRef(null);
+  //   når dette komponent indlæses, er betalingen ikke færdiggjort (false)
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
 
-  function submit(e) {
+  async function submit(e) {
     // forhindre submit i at refresh hele siden ved klik på knap
     e.preventDefault();
     theForm.current.elements.name.value;
     // object det bliver POST'et til database
-    insertOrder({
+    const response = await insertOrder({
       name: theForm.current.elements.name.value,
       email: theForm.current.elements.email.value,
       address: theForm.current.elements.address.value,
       basket: props.cart,
     });
+    if (response && response.length) {
+      setPaymentCompleted(true);
+    }
   }
 
   return (
-    <form onSubmit={submit} ref={theForm}>
-      <div className="form-control">
-        <label htmlFor="form-name">Name</label>
-        <input required type="text" name="name" id="form-name" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-email">E-mail</label>
-        <input required type="email" name="email" id="form-email" />
-      </div>
-      <div className="form-control">
-        <label htmlFor="form-address">Address</label>
-        <textarea required name="address" id="form-address" cols="20" rows="5"></textarea>
-      </div>
-      <button>Pay</button>
-    </form>
+    <>
+      {paymentCompleted ? (
+        <p>Thank you for shopping!</p>
+      ) : (
+        <form onSubmit={submit} ref={theForm}>
+          <div className="form-control">
+            <label htmlFor="form-name">Name</label>
+            <input required type="text" name="name" id="form-name" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-email">E-mail</label>
+            <input required type="email" name="email" id="form-email" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="form-address">Address</label>
+            <textarea required name="address" id="form-address" cols="20" rows="5"></textarea>
+          </div>
+          <button>Pay</button>
+        </form>
+      )}
+    </>
   );
 }
 
--- src/components/modules/db.js
+++ src/components/modules/db.js
@@ -1,8 +1,8 @@
-export function insertOrder(payload) {
+export async function insertOrder(payload) {
   const key =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vend0YmtvdmFsd25qcHB6ZWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg3MTksImV4cCI6MTk4MjE3NDcxOX0.T54ScdOq_PdkBUiFj41oy6MpG_hqs3xUwNBisvJ_2rI";
   const url = "https://oozwtbkovalwnjppzebk.supabase.co";
-  fetch(url + "/rest/v1/simpleshop", {
+  const res = await fetch(url + "/rest/v1/simpleshop", {
     method: "POST",
     headers: {
       apikey:
@@ -13,24 +13,6 @@ export function insertOrder(payload) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(payload),
-  })
-    .then()
-    .then();
+  });
+  return await res.json();
 }
-
-/*
-const options = {
-  method: 'POST',
-  headers: {
-    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vend0YmtvdmFsd25qcHB6ZWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg3MTksImV4cCI6MTk4MjE3NDcxOX0.T54ScdOq_PdkBUiFj41oy6MpG_hqs3xUwNBisvJ_2rI',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vend0YmtvdmFsd25qcHB6ZWJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg3MTksImV4cCI6MTk4MjE3NDcxOX0.T54ScdOq_PdkBUiFj41oy6MpG_hqs3xUwNBisvJ_2rI',
-    Prefer: 'return=representation'
-  },
-  body: '{"name":"Breaking Bad","genres":["crime","comedy","drama"],"seasons":5,"director":"Vince Gilligan","rating":4.9,"ongoing":false}'
-};
-
-fetch('https://oozwtbkovalwnjppzebk.supabase.co/rest/v1/tvShows', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-*/
